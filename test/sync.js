0152e6f005fdd8d09f09270c15b8ae9dfb5a7322
sadhu <rong8296@gmail.com>
2019-11-06 16:47:47 +0800

    重新组织代码，支持传入字段选项

--- test/sync.js
+++ test/sync.js
@@ -1,4 +1,4 @@
-const {loseWeight: thin} = require('../lose-weight')
+const {loseWeight} = require('../lose-weight')
 
 const source = [
   {
@@ -13,13 +13,55 @@ const source = [
     birth: '1900-0-0',
     '[{}]': 'nullnull',
     100: 100,
-    ' blank  ': 'blank'
+    ' blank  ': 'blank',
+    sex: 1
   },
   {x: 'x', y: 'y', z: 'z'}
 ]
 
+console.log(
+  loseWeight(source)
+    .opt({sex: ['女', '男']})
+    .handleBy(
+      function 姓名(item) {
+        if(item) return `username：${item.value[0]}`
+        return undefined
+      },
+      function age(item) {
+        if(item) return `${item}岁`
+        return undefined
+      },
+      // function birth(item, options) {
+      //   // console.log('birth item, options', item, options)
+      //   if(item) return `生日：${item}`
+      //   return undefined
+      // },
+      {},
+      null,
+      Symbol(),
+      false,
+      undefined,
+      0,
+      '[{}]',
+      100,
+      function() {},
+      '',
+      '    ',
+      ' blank  ',
+      function sex(item, options) {
+        // console.log('sex item, options', item, options)
+        // return `gender${item}`
+        if(typeof(item) === 'number') {
+          return options[item]
+        }
+        return undefined
+      }
+    )
+)
 
 
+/* 
+// 旧实现的测试用例
 const rt = thin(
   source,
   // Object, // 如果非要这样 hack，暂时无法有效阻止！
@@ -49,4 +91,6 @@ const rt = thin(
   ' blank  '
 )
 
-console.log(rt)
+console.log(rt) 
+
+*/
